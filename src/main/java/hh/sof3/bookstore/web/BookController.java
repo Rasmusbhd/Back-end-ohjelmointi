4e91715bc7efe86147c17858e68593f2d5c98f19
Rasmusbhd <bhd466@myy.haaga-helia.fi>
2024-02-05 19:13:48 +0200

    Third commit

--- src/main/java/hh/sof3/bookstore/web/BookController.java
+++ src/main/java/hh/sof3/bookstore/web/BookController.java
@@ -1,8 +1,11 @@
 package hh.sof3.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.sof3.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
@@ -11,4 +14,11 @@ public class BookController {
         return "index";
     }
 
+    @Autowired
+    private BookRepository repository;
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
+    }
 }
