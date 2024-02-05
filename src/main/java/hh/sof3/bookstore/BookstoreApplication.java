e0e713e9b1484c168a872c92cfc84cf5e21a2309
Rasmusbhd <bhd466@myy.haaga-helia.fi>
2024-02-05 18:29:20 +0200

    Second commit

--- src/main/java/hh/sof3/bookstore/BookstoreApplication.java
+++ src/main/java/hh/sof3/bookstore/BookstoreApplication.java
@@ -1,7 +1,13 @@
 package hh.sof3.bookstore;
 
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.sof3.bookstore.domain.Book;
+import hh.sof3.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -10,4 +16,16 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			Book b1 = new Book("A Farewell to Arms", "Ernest Hemingway", 1929, "1232323-21", 0.0);
+			Book b2 = new Book("Animal Farm", "George Orwell", 1945, "2212343-5", 0.0);
+
+			repository.save(b1);
+			repository.save(b2);
+		};
+	}
+
 }
+
